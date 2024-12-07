6bebd2e9067e5d7d45d7ce4d6924b25425359987
Rajaneesh93 <52157559+Rajaneesh93@users.noreply.github.com>
2024-12-07 16:47:57 +0530

    Create day8.py

---
+++ day8.py
@@ -0,0 +1,13 @@
+def grade_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
