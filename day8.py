f0f3864d2bc5d587fa119ba936a3c661743c2f2d
Rajaneesh93 <52157559+Rajaneesh93@users.noreply.github.com>
2024-12-08 06:21:12 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -11,3 +11,23 @@ def grade_status(score):
         return "D"
     else:
         return "F"
+
+
+#Leap Year Checker
+#Write a function check_leap_year(year) that takes an integer year and checks:
+
+#If the year is divisible by 4 but not divisible by 100, it's a leap year.
+#If the year is divisible by 400, it's a leap year.
+#Otherwise, it’s not a leap year.
+
+def leapyear(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 ==0):
+        print("its a Leap year")
+    else:
+        print("its not a leap year")
+
+year = int(input("Please enter year:"))
+
+print(leapyear(year))
+
+
